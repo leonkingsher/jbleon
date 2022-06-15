c67d0f207c327eec804811377f807d5b2f3d5bf1
leon sher <leon.sher88@gmail.com>
2022-06-15 10:15:48 +0300

    deploy it

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import birdimg from './img/bbird.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World, Ima fucking BIRD</h1>
+      <img src={birdimg} alt="birdibird"></img>
+    </div>
+  );
+}
+
+export default App;
